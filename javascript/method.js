356e29175275dd60270ed5c5fa40af5a4222d67a
Prabin <prabin02shr@gmail.com>
2024-09-02 08:04:59 +0545

    object method

---
+++ javascript/method.js
@@ -0,0 +1,34 @@
+// method
+/*
+typeof()
+console.log()
+prompt()
+Number()
+setTimeout()
+setInterval()
+*/
+
+// string:
+var text = "Hi guys";
+var skills = "c, c++, javascript, java, python";
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+// property
+console.log(text.length);
+console.log(text.substring(0, 2));
+console.log(text.substring(0, 5));
+// split
+console.log(skills.split(","));
+console.log(skills.split("c"));
+// includes
+var email = "ab@gmail.com";
+console.log(email.includes("@gmail.com"));
+
+// number:
+var n = 213.34212;
+console.log(n.toFixed("3"));
+console.log(n.toFixed("0"));
+
+// boolean
+// undefined
+// null
