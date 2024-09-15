9cb5e710d074014ba8bb6b928c549f0b130b9e61
Prabin <prabin02shr@gmail.com>
2024-09-15 08:10:08 +0545

    backend

---
+++ mern/backend/index.js
@@ -0,0 +1,16 @@
+/*
+1995
+ryan dahl => 2009
+node js
+framwork
+library
+run time enviroment
+
+npm => node pacakge manager
+
+
+file-file communication
+=> one file must export
+=> another file must import
+
+*/
