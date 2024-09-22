d9dc5bb2a59a4805ab59563b33e4bf52acd03d34
Prabin <prabin02shr@gmail.com>
2024-09-22 08:22:12 +0545

    middleware

--- mern/backend/app.js
+++ mern/backend/app.js
@@ -35,11 +35,38 @@ app.get("/login", function (request, response) {
     */
 });
 
+app.get("/user/update/:id", function (req, res) {
+  // res.json({
+  //   msg: "from user update",
+  //   user_id: req.params.id,
+  //   // queryData: req.query.hello,
+  //   username: req.query.username,
+  //   password: req.query.password,
+  // });
+
+var reg_username ="prabin"
+var reg_password = "1234"
+});
+
 app.get("/write/:filename/:content", function (request, response) {
   //   response.json({
   //     filename: request.params,
   //   });
   response.json(request.params);
+});
+
+// app.get("*", function (req, res) {
+//   res.json({
+//     msg: "Page not found",
+//     status: 404,
+//   });
+// });
+
+// regardless of any method and any url
+app.use(function (req, res) {
+  res.json({
+    msg: "Page not found",
+  });
 });
 
 app.listen(port, function (err, done) {
@@ -49,3 +76,12 @@ app.listen(port, function (err, done) {
     console.log("server listening at port: ", port);
   }
 });
+
+// middleware
+/*
+function(res, res){}
+
+app.use(middleware)
+
+app.use(funtion(req, res){})
+*/
