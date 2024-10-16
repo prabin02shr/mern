362dc753741dcc28389355c438c030d68d53d113
Prabin <prabin02shr@gmail.com>
2024-10-16 08:04:36 +0545

    update/ delete

--- mern/backend/controller/user.controller.js
+++ mern/backend/controller/user.controller.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const mongdb = require("mongodb");
-const mongoClient = mongdb.MongoClient;
+const mongodb = require("mongodb");
+const mongoClient = mongodb.MongoClient;
 const connectionURL = "mongodb://localhost:27017";
 const dbname = "group7db";
+const OId = mongodb.ObjectId;
 
 // const express = require("express");
 // const router = express.Router();
@@ -26,8 +27,8 @@ router.get("/view", function (req, res, next) {
       collection
         .find()
         .toArray()
-        .then(function(users){
-          res.json(users)
+        .then(function (users) {
+          res.json(users);
         })
         .catch(function (err) {
           return next(err);
@@ -41,19 +42,60 @@ router.get("/view", function (req, res, next) {
 // /user/
 router
   .route("/:user_id")
-  .get(function (req, res, next) {
-    res.json({
-      msg: "get single user details",
-    });
-  })
-  .post(function (req, res, next) {
-    res.json({});
-  })
+  // .get(function (req, res, next) {
+  //   res.json({
+  //     msg: "get single user details",
+  //   });
+  // })
+  // .post(function (req, res, next) {
+  //   res.json({});
+  // })
+  
   .put(function (req, res, next) {
-    res.json({});
+    mongoClient
+      .connect(connectionURL)
+      .then(function (client) {
+        var database = client.db(dbname);
+        var collection = database.collection("user");
+        collection
+          .updateOne(
+            {
+              _id: new OId(req.params.user_id),
+            },
+            {
+              $set: req.body,
+            }
+          )
+          .then(function (updateUser) {
+            res.json(updateUser);
+          })
+          .catch(function (err) {
+            return next(err);
+          });
+      })
+      .catch(function (err) {
+        return err;
+      });
   })
+
   .delete(function (req, res, next) {
-    res.json({});
+    mongoClient
+      .connect(connectionURL)
+      .then(function (client) {
+        var database = client.db(dbname);
+        var collection = database.collection("user");
+        collection
+          .deleteOne({ _id: new OId(req.params.user_id) })
+          .then(function (deleteUser) {
+            res.json(deleteUser);
+          })
+          .catch(function (err) {
+            return next(err);
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
   });
 
 module.exports = router;
